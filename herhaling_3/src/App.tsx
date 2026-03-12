6479c551bec0cc9d0ccae0d192e3c62e87f8281c
jonasvrnnn <jonas.verbruggen@student.ap.be>
2026-03-12 12:33:01 +0100

    folder change

--- herhaling_3/src/App.tsx
+++ herhaling_3/src/App.tsx
@@ -1,32 +1,23 @@
 //hide-start
-const styles = {
-  inputFields: {
-    display: "grid", gridTemplateColumns: "70px 200px"
-  }
-}
+import { useState } from "react";
 //hide-end
-const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-  console.log(event.target.value);
-}
-//hide-start
-const InputView = () => {
-  return (
-    <div>
-      <div style={styles.inputFields}>
-        <label htmlFor="name">name:</label>
-        <input type="text" id="name" onChange={handleChange} />
-        <label htmlFor="year">year:</label>
-        <input type="number" id="year" name="year" min="1900" max="2021" />
-      </div>
-      <input type="button" id="submit" value="Add Movie" />
-    </div>
-  )
-}
-//hide-end
-//hide-start
 const App = () => {
-  return <InputView />
-}
+  const [numbers, setNumbers] = useState<number[]>([0, 1, 2, 3, 4]);
 
+  return (
+    <>
+      <table>
+        <tbody>
+          {numbers.map((number, index) => (
+            <tr key={index}>
+              <td>{number}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+//hide-start
 export default App;
 //hide-end
