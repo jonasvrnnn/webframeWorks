fc1cb1b62c172fbcfec6baa4e87f91e8769d5b3e
jonasvrnnn <jonas.verbruggen@student.ap.be>
2026-03-14 17:10:53 +0100

    lab state loading indicator

--- labo4/lab-state-loading-indicator/src/App.tsx
+++ labo4/lab-state-loading-indicator/src/App.tsx
@@ -1,10 +1,30 @@
-
-function App() {
-
+import { useState } from "react"
+import { ColorRing } from 'react-loader-spinner'
+const LoadingIndicator = () => {
   return (
-    <>
-      <p>hello</p>
-    </>
+    <ColorRing
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="audio-loading"
+      wrapperStyle={{}}
+      wrapperClass="wrapper-class"
+      visible={true}
+    />
+  )
+}
+function App() {
+  const [loading, setloading] = useState(false);
+  const handleStartLoading: React.MouseEventHandler<HTMLButtonElement> = () => {
+    setloading(true)
+    setTimeout(() => {
+      setloading(false)
+    }, 3000)
+  }
+  return (
+    <div>
+      {loading ? <LoadingIndicator /> : <button onClick={handleStartLoading}>start loading</button>}
+    </div>
   )
 }
 
